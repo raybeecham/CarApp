00f431d38297031cb4bd340a5679669bba666d06
raybeecham <raybeecham2009@gmail.com>
2019-02-23 02:50:21 -0600

    Update CabFare.java

--- app/src/main/java/edu/txstate/rpb31/carapp/CabFare.java
+++ app/src/main/java/edu/txstate/rpb31/carapp/CabFare.java
@@ -13,53 +13,7 @@ import java.text.DecimalFormat;
 
 public class CabFare extends MainActivity {
 
-    double initialFee = 3.00;
-    double mileageRate = 3.25;
-    double numberOfMiles;
-    double totalCost;
-    String groupChoice;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cab_fare);
-
-//       final EditText miles = (EditText) findViewById(R.id.txtNumberOfMiles);
-            final Spinner spnCarType = (Spinner) findViewById(R.id.spnCarType);
-            final TextView result = findViewById(R.id.txtFareResult);
-            Button displayCost = (Button) findViewById(R.id.btnCalculateFare);
-            final EditText txtNumberOfMiles = findViewById(R.id.txtNumberOfMiles);
-            displayCost.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String strNumberOfMiles = txtNumberOfMiles.getText().toString();
-                    int intNumberOfMiles = 0;
-                    try {
-                        intNumberOfMiles = Integer.parseInt(strNumberOfMiles);
-                    } catch (Exception ex) {
-                   result.setText("No entry detected please enter a numerical value.");
-                        Toast.makeText(CabFare.this, "Please enter a number", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-
-               totalCost = initialFee + intNumberOfMiles * mileageRate;
-               DecimalFormat currency = new DecimalFormat("$###,###.00");
-               groupChoice = spnCarType.getSelectedItem().toString();
-
-                    if (intNumberOfMiles == 0) {
-                        Toast.makeText(CabFare.this, "You cannot travel 0 Miles. Please enter a valid number ", Toast.LENGTH_LONG).show();
-                    } else {
-                        displayResults(intNumberOfMiles, currency, result);
-
-                    }
-                }
-            });
-        }
-
-    public void displayResults(int intNumberOfMiles, DecimalFormat currency, TextView result) {
-        result.setText("You have requested a " + groupChoice + " to travel " +
-                intNumberOfMiles +" miles,\n so the cost will be " + currency.format(totalCost));
-    }
+    Will post code when project is completed so no cheating occurs.
 }
 
 
